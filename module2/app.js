e0f4abab75521071a622afced8cd5237b6e820c4
slowdata <slowmotion@outlook.com>
2017-03-05 23:45:04 +0000

    Coursera SPA assignment 2 completed

--- module2/app.js
+++ module2/app.js
@@ -5,19 +5,68 @@
         angular.module("ShoppingListCheckOff", [])
             .controller("ToBuyController", ToBuyController)
             .controller("AlreadyBoughtController", AlreadyBoughtController)
-            .service();
+            .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
         
-        function ToBuyController () {
+        ToBuyController.$inject = ["ShoppingListCheckOffService"];
+        function ToBuyController (ShoppingListCheckOffService) {
             var toBuy = this;
 
-            toBuy.teste = "funciona? - SIM";
+            toBuy.items = ShoppingListCheckOffService.getToBuyList();
+
+            toBuy.removeItem = function(item) {
+                ShoppingListCheckOffService.buyItem(item);
+            }
+
         }
 
-        function AlreadyBoughtController() {
+        AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"]
+        function AlreadyBoughtController(ShoppingListCheckOffService) {
             var alreadyB = this;
 
-            alreadyB.teste = "Coisas";
+            alreadyB.items = ShoppingListCheckOffService.getBoughtList();
+        }
+
+
+
+        function ShoppingListCheckOffService() {
+            var service = this;
+
+            var tobuy = [{
+                name: "cookie bags",
+                quantity: 10
+            },{
+                name: "red wine bottles",
+                quantity: 2
+            },
+            {
+                name: "chip bags",
+                quantity: 1
+            },
+            {
+                name: "cheese",
+                quantity: 2
+            },
+            {
+                name: "milk",
+                quantity: 1
+            }];
+            
+            var bought = [];
+
+            service.getToBuyList = function () {
+                return tobuy;
+            };
+
+            service.getBoughtList = function () { 
+                return bought;
+            };
+
+            service.buyItem = function (itemIndex) {
+                bought.push(tobuy[itemIndex]);
+                tobuy.splice(itemIndex, 1);
+            }
+
         }
     }
 )();
