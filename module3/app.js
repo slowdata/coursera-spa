54b7199f0b554769e383e29a2b28526af3ca5a35
Ricardo Dias <ii0602@iies.seg-social.pt>
2017-03-13 17:29:30 +0000

    Bootstrap assignment 3 and make progress with call $http

---
+++ module3/app.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('URL', 'https://davids-restaurant.herokuapp.com/menu_items.json');
+
+
+    NarrowItDownController.$inject = ['MenuSearchService']
+    function NarrowItDownController(MenuSearchService) {
+        var Ctrl = this;
+
+        Ctrl.getMenuItems = function() {
+            var promise = MenuSearchService.getMatchedMenuItems();
+        
+            promise.then(function(response){
+                Ctrl.found = response.data.menu_items;
+                console.log(Ctrl.found);
+            })
+            .catch(function(error){
+                console.log("Error", error);
+            });
+
+        };
+        
+
+    }
+
+
+    MenuSearchService.$inject  =['$http', 'URL']
+    function MenuSearchService($http, URL) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+
+            var response = $http({
+                method: "GET",
+                url: URL
+            });
+
+            return response;
+        };
+    }
+
+
+})();
